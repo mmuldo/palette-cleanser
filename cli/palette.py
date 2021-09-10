b7cf7d5c6a1876aec64a12856ba9fc938caf9447
Matt Muldowney <matt.muldowney@gmail.com>
2021-09-10 16:49:56 -0500

    adding other parameters to generate

--- cli/palette.py
+++ cli/palette.py
@@ -1,6 +1,6 @@
 from palettecleanser import palette as pal
 from palettecleanser import config
-from typing import Optional
+from typing import Optional, Any
 from PIL import Image, UnidentifiedImageError
 
 import typer
@@ -45,12 +45,20 @@ def create(
         p.save()
 
 
-# TODO: add other arguments for pal.from_image
-def generate_from_image(image_path: str, name: Optional[str] = None):
-    '''generates palette from image'''
+def generate_from_image(
+        image_path: str,
+        name: Optional[str] = None,
+        quantize_number: int = 64,
+        algorithm: str = 'from_ordered_colors_match',
+        **algorithm_parameters
+):
+    '''generates palette from image
+
+    see palettecleanser.palette.from_image for more details
+    '''
     try:
         with Image.open(image_path) as img:
-            p = pal.from_image(img, name)
+            p = pal.from_image(img, name, quantize_number, pal.generation_algorithms[algorithm], **algorithm_parameters)
     except FileNotFoundError:
         print(f"No such file or directory: '{image_path}'", file=sys.stderr)
         raise typer.Exit(1)
@@ -63,16 +71,26 @@ def generate_from_image(image_path: str, name: Optional[str] = None):
         p.save()
 
 
+# TODO: --help type flag for available algorithms
 @app.command(help=f'''generates a palette
 
 if --name option is passed, saves palette to {config.palettes_dir}
 where it can be manually edited later''')
 def generate(
         from_image: str = typer.Option('', metavar='PATH', help='generate palette from image at the specified path'),
-        name: Optional[str] = typer.Option(None, metavar='NAME', help=f'saves the palette to "{config.palettes_dir}" with specified name')
+        name: Optional[str] = typer.Option(None, metavar='NAME', help=f'saves the palette to "{config.palettes_dir}" with specified name'),
+        quantize_number: int = typer.Option(64, metavar='QUANTIZE_NUMBER', help='if generating from image, specifies the number of colors to quantize the image to and thus select colors from; --from-image option must be passed'),
+        algorithm: str = typer.Option('from_ordered_colors_match', metavar='ALGORITHM', help='if generating from image, specifies thealgorithm to use for converting a list of colors to a palette;  --from-image option must be passed'),
+        algorithm_parameter: Optional[list[str]] = typer.Option(None, metavar='KEY=VALUE', help='if generating from image, specifies keyword arguments which algorithm accepts in addition to a list of colors and name of palette; --algorithm option must be passed')
 ):
     if from_image:
-        generate_from_image(from_image, name)
+        generate_from_image(
+            from_image,
+            name,
+            quantize_number,
+            algorithm,
+            **{k: v for k, v in [param.split('=') for param in algorithm_parameter]}
+        )
 
 
 @app.command()
--- cli/theme.py
+++ cli/theme.py
@@ -1,7 +1,8 @@
 from palettecleanser import theme
 from palettecleanser import template
 from palettecleanser import config
-from typing import Optional
+from palettecleanser import palette as pal
+from typing import Optional, Any
 from PIL import Image, UnidentifiedImageError
 
 import typer
@@ -47,24 +48,36 @@ def deploy(
     template.template_managed(t)
 
 
-# TODO: add settings argument
 @app.command(help=f'''creates theme from a list of palettes, name, image path, and additional settings
 and saves to {config.themes_dir} where it can be manually edited later''')
 def create(
         palettes: list[str] = typer.Argument(..., help='space-delimited list of names of saved palettes'),
         name: str = typer.Option(..., metavar='NAME', help='name of theme'),
         image_path: str = typer.Option(..., metavar='PATH', help='path to background image'),
+        setting: Optional[list[str]] = typer.Option(None, metavar='KEY=VALUE', help='additional settings to initialize new theme with')
 ):
-    t = theme.Theme(name, palettes, image_path)
+    t = theme.Theme(name, palettes, image_path, {k: v for k, v in [single_setting.split('=') for single_setting in setting]})
     print(t)
     t.save()
 
-# TODO: add other arguments for theme.from_image
-def generate_from_image(image_path: str, name: str):
-    '''generates theme from image'''
+def generate_from_image(
+        image_path: str,
+        name: str,
+        settings: Optional[dict[str, Any]] = None,
+        quantize_number: int = 64,
+        algorithm: str = 'from_ordered_colors_match',
+        **algorithm_parameters
+):
+    '''generates theme from image
+
+    see palettecleanser.theme.from_image for more details
+    '''
+    if not settings:
+        settings = {}
+
     try:
         with Image.open(image_path) as img:
-            t = theme.from_image(img, name)
+            t = theme.from_image(img, name, settings, quantize_number, pal.generation_algorithms[algorithm], **algorithm_parameters)
     except FileNotFoundError:
         print(f"No such file or directory: '{image_path}'", file=sys.stderr)
         raise typer.Exit(1)
@@ -76,15 +89,26 @@ def generate_from_image(image_path: str, name: str):
     t.save()
 
 
+# TODO: --help type flag for available algorithms
 @app.command(help=f'''generates a theme and saves to
 {config.themes_dir} where it can be manually edited later''')
 def generate(
         from_image: str = typer.Option('', metavar='PATH', help='generate theme from image at the specified path'),
-        name: Optional[str] = typer.Option(None, metavar='NAME', help=f'name of theme')
+        name: str = typer.Option(..., metavar='NAME', help=f'name of theme'),
+        setting: Optional[list[str]] = typer.Option(None, metavar='KEY=VALUE', help='additional settings to initialize new theme with'),
+        quantize_number: int = typer.Option(64, metavar='QUANTIZE_NUMBER', help='if generating from image, specifies the number of colors to quantize the image to and thus select colors from; --from-image option must be passed'),
+        algorithm: str = typer.Option('from_ordered_colors_match', metavar='ALGORITHM', help='if generating from image, specifies thealgorithm to use for converting a list of colors to a palette;  --from-image option must be passed'),
+        algorithm_parameter: Optional[list[str]] = typer.Option(None, metavar='KEY=VALUE', help='if generating from image, specifies keyword arguments which algorithm accepts in addition to a list of colors and name of palette; --algorithm option must be passed')
 ):
-    '''generates a theme'''
     if from_image:
-        generate_from_image(from_image, name)
+        generate_from_image(
+            from_image,
+            name,
+            {k: v for k, v in [single_setting.split('=') for single_setting in setting]},
+            quantize_number,
+            algorithm,
+            **{k: v for k, v in [param.split('=') for param in algorithm_parameter]}
+        )
 
 
 @app.command()
